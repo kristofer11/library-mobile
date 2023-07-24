f6cdc562a112550b9e66fbb28a6b4164b5cff006
Kris Hvattum <klhvattum@live.com>
2023-07-23 23:23:10 -0700

    basic setup

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import { React } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.header}>
+            <Text style={styles.boldText}>
+                Hello World
+            </Text>
+        </View>
+        <View style={styles.body}>
+            <Text>lorem lorem lorem lorem lorem </Text>
+            <Text>lorem lorem lorem lorem lorem </Text>
+            <Text>lorem lorem lorem lorem lorem </Text>
+        </View>
     </View>
   );
 }
@@ -17,4 +26,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }
 });
