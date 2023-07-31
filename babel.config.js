808510c6c8b9c5384fb03822950db1cdbf855dd4
Kris Hvattum <klhvattum@live.com>
2023-07-31 13:47:15 -0700

    update babel.config and update package.json

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
+    api.cache(true);
+    return {
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: ['react-native-reanimated/plugin'],
+    };
   };
-};
+
+
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//   };
+// };
